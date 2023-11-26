b604d066107614c22371906dcf04880e7f241448
Felipe Velandia <Felipe Velandia>
2023-11-25 21:49:59 -0500

    Adding common file

--- Sources/CommonPackageFolder/CommonPackageFolder.swift
+++ Sources/CommonPackageFolder/CommonPackageFolder.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class CommonPackageFolder {
+	public init() {
+		print("Common.init()")
+	}
+	
+	public func commonFunction() {
+		print("Common.commonFunction()")
+	}
+}
